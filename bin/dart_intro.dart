67bc89bb1de854d3d52fc650533f923cb8726fca
Yamen Khazar <155854770+Yamen-2004@users.noreply.github.com>
2024-02-10 11:45:41 +0300

    start with datatype in dart

--- bin/dart_intro.dart
+++ bin/dart_intro.dart
@@ -1,5 +1,19 @@
-import 'package:dart_intro/dart_intro.dart' as dart_intro;
+void main() {
+  String name = "yamen";
+  print(name);
+  int age = 20;
+  print(age);
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_intro.calculate()}!');
+  double time = 11.37;
+  print(time);
+  bool isMale = true;
+  print(isMale);
+   dynamic hello = null ;
+  print(hello);
+  List<int> list1 = [1, 2, 3, 4, 5, 60];
+  print(list1);
+  List list2 = ["yamen ", 2, true];
+  print(list2);
+   Map map = {"username": "yamen " , "older": 20 };
+   print (map);
 }
