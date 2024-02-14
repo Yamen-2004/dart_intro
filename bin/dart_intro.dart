6d46bf2a39bb3c6327a6510a885d53e68b27a366
Yamen Khazar <155854770+Yamen-2004@users.noreply.github.com>
2024-02-15 02:06:49 +0300

    commit

--- bin/dart_intro.dart
+++ bin/dart_intro.dart
@@ -1,5 +1,43 @@
+// import "dart:io";
 void main (){
-print ("dou");
+// var i = 0;
+
+// var choice = stdin.readLineSync()!;
+
+// while (true){
+//   print ("hello to my app");
+//   print ("press 1 to draw a traingle with for loop");
+//   print ("press 2 to decide if the number odd or even ");
+//   print ("press 3 to switch  ");
+//   print ("press 4 to exit ");
+
+// if(choice == 1){
+//   for (i= 0 ;i<5;i++ ){
+//     print ("*"*i);
+//   }
 
 
-}
+// }
+// if (choice == 2){
+//  dynamic x = stdin.readLineSync()!;
+// if (x%2==0){
+//   print ("the number is vwn ");
+// }
+// if (x%3!=2){
+//   print ("the number is odd ");
+// }
+
+// }
+// if(choice == 3){
+//   print ("hc");
+
+// }
+// if (choice == 4){
+//   break;
+
+// } break ;
+
+
+// }
+
+ }
