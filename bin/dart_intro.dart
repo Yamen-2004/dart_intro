2046394468e5a130fb628d131522ee074fdcd779
Yamen Khazar <155854770+Yamen-2004@users.noreply.github.com>
2024-02-15 13:24:02 +0300

    task

--- bin/dart_intro.dart
+++ bin/dart_intro.dart
@@ -1,43 +1,43 @@
-// import "dart:io";
-void main (){
-// var i = 0;
+void main() {
+  int a = 0;
+  int choice = 3;
+  int number = 3;
+  int my_bydget = 200;
 
-// var choice = stdin.readLineSync()!;
+  while (a < 2) {
+    a += 2;
+    print("press 1 to draw a traingle using stars  ");
+    print("press 2 to check if the number is even ");
+    print("press 4 if you want to see about price cases in apple mobiles  ");
+    print("press 4 to exit ");
 
-// while (true){
-//   print ("hello to my app");
-//   print ("press 1 to draw a traingle with for loop");
-//   print ("press 2 to decide if the number odd or even ");
-//   print ("press 3 to switch  ");
-//   print ("press 4 to exit ");
-
-// if(choice == 1){
-//   for (i= 0 ;i<5;i++ ){
-//     print ("*"*i);
-//   }
-
-
-// }
-// if (choice == 2){
-//  dynamic x = stdin.readLineSync()!;
-// if (x%2==0){
-//   print ("the number is vwn ");
-// }
-// if (x%3!=2){
-//   print ("the number is odd ");
-// }
-
-// }
-// if(choice == 3){
-//   print ("hc");
-
-// }
-// if (choice == 4){
-//   break;
-
-// } break ;
-
-
-// }
-
- }
+    if (choice == 1) {
+      for (var i = 0; i < 5; i++) {
+        print("*" * i);
+      }
+    } else if (choice == 2) {
+      if (number % 2 == 0) {
+        print("the number is even");
+      } else {
+        print("the number is odd");
+      }
+    } else if (choice == 3) {
+      switch (my_bydget) {
+        case 50:
+          print("no iphone with this price");
+        case 100:
+          print("bye iphone sex");
+        case 150:
+          print("bye iphoone 7");
+        case 200:
+          print("iphone x ");
+        default:
+          print("we are sorry you can't bye ");
+      }
+    } else {
+      break;
+    }
+  }
+}
+// this is my task mr Mohammad , but i found problem with using terminal in input so I can't gave you what you want correctly 
+// I think I should use another IDE , will decide later 
